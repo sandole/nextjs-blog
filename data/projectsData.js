a8cd89c46f2633f2158362ac33768450a783cbca
sandole <sandole97@gmail.com>
2022-01-19 17:09:03 -0700

    add new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,12 @@ const projectsData = [
     imgSrc: '/static/images/Honeyjam.png',
     href: 'https://www.honeyjam.ca',
   },
+  {
+    title: 'The Odin Project',
+    description: `Open source online curriculum for learning full stack development`,
+    imgSrc: '/static/images/TOP.png',
+    href: 'https://www.theodinproject.com/',
+  },
 ]
 
 export default projectsData
