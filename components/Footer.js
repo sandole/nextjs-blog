89e7a47171f8d923fc1ecc3612fb69854c9cf957
sandole <sandole97@gmail.com>
2022-01-30 02:19:49 -0700

    bugfix

--- components/Footer.js
+++ components/Footer.js
@@ -5,13 +5,19 @@ export default function Footer() {
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
+      
         <div className="flex mb-3 space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
+          <Image
+            alt="support"
+            src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=🚀&slug=sandole97&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff"
+            href="https://www.buymeacoffee.com/sandole97"
+            width="200" height="70"
+          />
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-        <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="sandole97" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
           <div>{siteMetadata.author}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
