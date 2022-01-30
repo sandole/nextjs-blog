7a24ef6a97144e46eb9de4d6d9e8a1f15be29b9f
sandole <sandole97@gmail.com>
2022-01-30 03:02:38 -0700

    attempt to fix bug

--- components/Footer.js
+++ components/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
+          <SocialIcon kind="github" href={siteMetadata.github} />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} />
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
