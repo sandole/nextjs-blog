329615e3988529d9ab79dd7c143a6715f74a581f
sandole <sandole97@gmail.com>
2022-01-30 03:25:30 -0700

    footer changes

--- components/Footer.js
+++ components/Footer.js
@@ -7,15 +7,6 @@ export default function Footer() {
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
-          <SocialIcon kind="github" href={siteMetadata.github} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} />
-        </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-        </div>
         <div className="mb-8">
           <Link href="https://www.buymeacoffee.com/sandole97">
             <Image
@@ -26,6 +17,15 @@ export default function Footer() {
               layout="responsive"
             />
           </Link>
+        </div>
+        <div className="flex mb-3 space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
+          <SocialIcon kind="github" href={siteMetadata.github} />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} />
+        </div>
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{siteMetadata.author}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
       </div>
     </footer>
