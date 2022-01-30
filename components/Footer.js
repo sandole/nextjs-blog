21f686a9aeee19010f610b0884739614eb610c22
sandole <sandole97@gmail.com>
2022-01-30 02:33:04 -0700

    restore to prior version

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
-import Image from 'next/image'
 
 export default function Footer() {
   return (
@@ -10,13 +9,6 @@ export default function Footer() {
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
-          <Image
-            alt="support"
-            src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=🚀&slug=sandole97&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff"
-            href="https://www.buymeacoffee.com/sandole97"
-            width="235"
-            height="50"
-          />
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
