3850798b0348f973421ca01aa771fbdfa5e34d68
sandole <sandole97@gmail.com>
2022-01-30 03:33:52 -0700

    bugfix

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
-        <div>
+        <div className="flex">
           <Link href="https://www.buymeacoffee.com/sandole97">
             <Image
               alt="Support Me"
