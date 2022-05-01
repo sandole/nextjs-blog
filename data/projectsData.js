54daeeb25529a6d2b94543780e570da454d3ae18
sandole <sandole97@gmail.com>
2022-04-30 20:45:37 -0600

    blog edit

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,21 +1,28 @@
 const projectsData = [
   {
     title: 'Honeyjam',
-    description: `Website development for a local restaurant`,
+    description: `Website development for a local restaurant using MERN stack and API to Square, Google Pay and etc`,
     imgSrc: '/static/images/Honeyjam.png',
     href: 'https://www.honeyjam.ca',
   },
   {
     title: 'The Odin Project',
-    description: `Open source online curriculum for learning full stack development`,
+    description: `Open source online curriculum for learning full stack development: from basic HTML to MERN stack`,
     imgSrc: '/static/images/TOP.png',
     href: 'https://www.theodinproject.com/',
   },
   {
-    title: 'Minecraft Server',
-    description: `Minecraft server hosted on Azure VM: join swbzminecraftserver.westus3.cloudapp.azure.com`,
+    title: 'Minecraft Server using Azure',
+    description: `Minecraft server fully hosted on Azure, from building vm image to pipelining and deploying: join swbzminecraftserver.westus3.cloudapp.azure.com`,
     imgSrc: '/static/images/minecraft.png',
     href: 'https://johnseong.vercel.app/blog/AzureVMApplication',
+  },
+  {
+    title:
+      'University Capstone Project: Correction of Weightlifting Techniques using Image Processing',
+    description: `Research and application of human anatomy during three major Olympic Lifting movements and using MATLAB to implement image processing via machine vision and displaying useful information using GUI with appropriate OOP`,
+    imgSrc: '/static/images/MATLAB-logo.png',
+    href: '',
   },
 ]
 
