f3142af17b0b3fdc0378171719e3f79a5e39f845
John Seong <39040639+sandole@users.noreply.github.com>
2022-10-16 13:38:55 -0600

    remove unnecessary

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -6,12 +6,6 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 
-const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
-const discussUrl = (slug) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(
-    `${siteMetadata.siteUrl}/blog/${slug}`
-  )}`
-
 const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
